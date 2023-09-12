26189fe7db3031adfd5b525172e6cc7380eb3c91
Noah Maquet <nmaquet@fu-r8-p4.s19.be>
2023-09-12 09:11:33 +0200

    save

---
+++ philo/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaquet <nmaquet@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 13:28:12 by nmaquet           #+#    #+#             */
+/*   Updated: 2023/09/11 13:32:26 by nmaquet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "libft.h"
+# include <stdio.h>
+# include <sys/time.h>   // for gettimeofday()
+# include <unistd.h>     // for sleep()
+
+
+
+#endif
---
+++ philo/srcs/philo.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaquet <nmaquet@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 10:23:34 by nmaquet           #+#    #+#             */
+/*   Updated: 2023/09/11 10:27:16 by nmaquet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	number_of_philosophers;
+int	time_to_die;
+int	time_to_eat;
+int	time_to_sleep;
+int	number_of_times_each_philosopher_must_eat;
+int	number_of_forks;
+
+number_of_forks = number_of_philosophers;
